a1ea0154e706db10ce2ddbc2ae8b520832f906dc
Daniel Ramirez <danyeldkn@gmail.com>
2022-10-29 19:36:35 -0500

    primer commit

---
+++ ecomerce-comision34800/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'NavBar.css'
+
+const NavBar = () => {
+  return (
+    <nav class="bar">
+        <ul>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Nosotros</li>
+            <li>Imagenes</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
